dee1da0600e4a3ea1766d81731689e0812281d6a
yumi20124 <150638433+yumi20124@users.noreply.github.com>
2024-04-29 11:58:14 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,7 +15,11 @@ function setup() {
   radioElement.position(width/2-300,20)
   radioElement.option("方塊")
   radioElement.option("圓圈")
-  radioElement.style("color","30")
+  radioElement.option("亮度")
+  radioElement.option("紅底")
+  radioElement.option("文字")
+  radioElement.style("color","#fff")
+  radioElement.style("font-size","30px")
 }
 
 function draw() {
@@ -29,12 +33,50 @@ function draw() {
       for(var y=0;y<captureGraphics.height;y=y+20){
         var pixel = captureGraphics.get(x,y)
         fill(pixel)
-        if(radioElement.value()=="方塊"){
+        if(radioElement.value()=="方塊" || radioElement.value()==""){
           rect(x,y,span)
-          }
-          if(radioElement.value()=="圓圈"){}
+        }
+        if(radioElement.value()=="圓圈"){
           ellipse(x,y,span)
+        }
+        if(radioElement.value()=="亮度"){
+          bk = (pixel[0]+pixel[1]+pixel[2])/3
+          fill(bk)
+          ellipse(x,y,span) //span代表圓圈直徑
+        }
+        if(radioElement.value()=="紅底"){
+          colorMode(HSB)
+          fill(pixel[0],80,80)
+          push()
+            // rotate(pixel[0]/100)
+            rectMode(CENTER)
+            rect(x,y,span*0.8)
+            fill(0)
+            ellipse(x,y,10) //span代表圓圈直徑
+            pop()
+        }
+        if(radioElement.value()=="文字"){
+          colorMode(HSB)
+          fill(pixel[0],80,80)
+          push()
+            // rotate(pixel[0]/100)
+            rectMode(CENTER)
+            rect(x,y,span*0.8)
+            fill(0)
+            ellipse(0,0,10) //span代表圓圈直徑
+            pop()
+            
+        }
+        if(radioElement.value()=="文字"){
+        const density="1522"
+        let txt ="一二三四我"
+        bk=(pixel[0]+pixel[1]+pixel[2])/3.
+        let bkld=int(map(bk,0,255,9,0))
+        text(txt[bkld],x,y)
+        textSize(span)
+        }
       }
     }
   pop()
-}
+  }
+
