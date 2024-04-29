b9ecb2ded7dbe127bdd9dc4340a71d06afb70bc3
yumi20124 <150638433+yumi20124@users.noreply.github.com>
2024-04-29 09:41:43 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  capture = createCapture(VIDEO)  //啟動攝影機
+  capture.size(320,240);//設定顯示畫面大小
+
+}
+
+function draw() {
+  background(220);
+  push()
+  translate(width/2-160,height/2-120)  //把原點移到
+  image(capture,0,0)  //在()
+  pop()
+}
